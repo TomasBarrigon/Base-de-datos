4d487d478a6be03f0c5a8a6ef8845cac45009db5
Tomás Barrigón <81708768+pandelo13@users.noreply.github.com>
2021-04-30 14:16:30 -0300

    Delete Tarea.sql

--- Tarea.sql
+++
@@ -1,7 +0,0 @@
-Create database personas;
-use personas;
-Create table alumnos(nombre varchar(50),edad int);
-Insert into alumnos(nombre,edad) values("equis",12);
-Select nombre from alumnos;
-Select edad from alumnos;
-Select * from alumnos;
