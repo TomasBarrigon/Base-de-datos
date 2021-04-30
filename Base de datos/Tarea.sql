ec28fe63f1737a618eb407fae6ed1dc6cf0d54ae
Tomás Barrigón <81708768+pandelo13@users.noreply.github.com>
2021-04-30 14:17:07 -0300

    Add files via upload

---
+++ Base de datos/Tarea.sql
@@ -0,0 +1,7 @@
+Create database personas;
+use personas;
+Create table alumnos(nombre varchar(50),edad int);
+Insert into alumnos(nombre,edad) values("equis",12);
+Select nombre from alumnos;
+Select edad from alumnos;
+Select * from alumnos;
